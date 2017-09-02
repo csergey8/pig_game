e9b8ff23c5ae5db387563915d602afdc6f5afc11
csergey8 <c.sergey@protonmail.com>
2017-09-02 20:22:11 +0300

    add dice dom and clear results

--- app.js
+++ app.js
@@ -8,3 +8,35 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var score, roundScore, activePlayer;
+
+score = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  var dice = Math.floor((Math.random() * 6) + 1);
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+
+  if(dice !== 1){
+  roundScore = roundScore + dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer++;
+  }
+
+
+});
